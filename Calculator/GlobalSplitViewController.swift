e567bd65ec371f4cf958c857fc0e378e71ad764f
Tzu-Hao Wei <socrates.wei@gmail.com>
2016-02-11 17:12:35 +0000

    Add GlobalSplitViewController to display master initially

---
+++ Calculator/GlobalSplitViewController.swift
@@ -0,0 +1,37 @@
+//
+//  GlobalSplitViewController.swift
+//  Calculator
+//
+//  Created by Tzu-Hao Wei on 2/11/16.
+//  Copyright © 2016 Tzu-Hao Wei. All rights reserved.
+//
+
+import UIKit
+
+class GlobalSplitViewController: UISplitViewController, UISplitViewControllerDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        // Do any additional setup after loading the view.
+    }
+    func splitViewController(splitViewController: UISplitViewController, collapseSecondaryViewController secondaryViewController: UIViewController, ontoPrimaryViewController primaryViewController: UIViewController) -> Bool {
+        return true
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
